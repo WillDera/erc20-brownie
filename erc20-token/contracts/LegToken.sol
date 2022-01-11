c6d3fd648c50529f86f7b4ca0c8a796ae011ff40
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-01-11 23:17:29 +0100

    Leg Token (ERC20)
    - Created custom token (Leg)
    - Defined token total supply
    - Deployed token to rinkeby on eth network
    - added liquidity for token on uniswap, pair = LG/ETH
    - Current price 1 LG = 0.0099 ETH

--- erc20-token/contracts/LegToken.sol
+++ erc20-token/contracts/LegToken.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract LegToken is ERC20 {
+    // total supply in wei
     constructor(uint256 initialSupply) ERC20("Leg", "LG") {
         _mint(msg.sender, initialSupply);
     }
